b55d33dc5a874b3503191822d9aac5933125e273
Puso Ramoroa <43412934+Puso90@users.noreply.github.com>
2024-09-06 18:18:23 +0200

    new save and delete numbers feature
    Ability to save, and delete by clicking on numbers

--- share-save_buttons.js
+++ share-save_buttons.js
@@ -5,60 +5,73 @@ const shareNum = document.querySelector('.shareBtn');
 //const storing = document.getElementById('storing');
 const listContainer = document.querySelector('ul');
 //const numbersGenerated = document.querySelector('.number-generator');
-const popUp2 = document.getElementsByClassName('.icon2');
+const show = document.querySelector('.savedNumbers');
+
+
+/*
+const date = new Date();
+
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            let year = date.getFullYear();
+            let second = date.getSeconds();
+
+            // This arrangement can be altered based on how we want the date's format to appear.
+            let currentDate = `${day}-${month}-${year}-${second}`;
+            console.log(currentDate); // "17-6-2022"
+*/
 
 
 /*______ THE PROCESS - SAVE SHARE ______*/
 
 // Save Button, saves the number generated
-saveBtn.addEventListener('click', () => {
+          saveBtn.addEventListener('click', () => {
 
-//If statement not working - MUST FIX - No save if numbers are not generated!
-    if (!numbers) {
-        window.alert('Coming soon!')
-        return;
-    }
+              const saved = document.createElement('li');
+                
+                localStorage.setItem(localStorage.length, JSON.stringify(numbers.slice(0,7).join(' ,  ')) )
+                  listContainer.appendChild(saved); 
+                  saved.classList.add('savedNumbers')
+                  // think I need to somehow link <li>HTML to key.storage
+                  saved.innerHTML = JSON.stringify(numbers.slice(0,7).join(' ,  '));
+                  
 
-    const saved = document.createElement('li');
-        saved.textContent = numbers.slice(0,7).join(' ,  ');
-        listContainer.appendChild(saved); 
-        saved.classList.add('savedNumbers')
-        localStorage.setItem(localStorage.length, numbers.slice(0,7).join(' ,  ') )
-
-        
-    saved.addEventListener('click', () => {
-        listContainer.removeChild(saved);
-       
-    }) 
-
-    // window.alert('Save button, coming soon!'); 
-    console.log(`saveing...`); // save icon works
-});
+              saved.addEventListener('click', () => {
+              
+                  listContainer.removeChild(saved);
+                  localStorage.removeItem(`${[localStorage.key([0],[1], [2], [3], [4], [5])]}`);
+                  
+              }) 
+              // window.alert('Save button, coming soon!'); 
+              console.log(`saveing...`); // save icon works
+          });
 
 
-/*______ SHARE BUTTON => TO THE WORLD ______*/
 
-//Listen to share icon clicks
-shareNum.addEventListener('click', () => {
-    //Share numbers or app link on web with api
-        navigator.share({
-        url: document.URL,
-        title: document.title,
-        text: "Look up lucky lotto numbers, Boom.. Enjoy your luck!"
-      });
+          /*______ SHARE BUTTON => TO THE WORLD ______*/
 
-    console.log(`sharing...`);
-});
+          //Listen to share icon clicks
+          shareNum.addEventListener('click', () => {
+              //Share numbers or app link on web with api
+                  navigator.share({
+                  url: document.URL,
+                  title: document.title,
+                  text: "Look up lucky lotto numbers, Boom.. Enjoy your luck!"
+                });
+
+              console.log(`sharing...`);
+          });
+
+
+
 
 /* INFORM LISTS POPUP */
 
-
-
 // Toggle visibility on click
 // NOT WORKING - ERROR!!!!!!!!
-function aboutPopUp() {
-    document.getElementsByClassName('icon, popup').style.display = 'block';
-}
+  function aboutPopUp() {
+      document.getElementsByClassName('icon, popup').style.display = 'block';
+  }
     //Strange must work on this more
 
 
@@ -98,8 +111,7 @@ ________________________________________________________________________________
 
 //_________________________________________________________________________________________________________
            // Creator: Puso Moses Ramoroa
-//_________________________________________________________________________________________________________
-           
+          
 
 /*__________________________________________________________________________________________________________
 
@@ -121,4 +133,5 @@ ________________________________________________________________________________
 
     2.      Must probably consider using actual unique ID rather 
             then linking that to a date saved of day-month-year
+
 _____________________________________________________________________________________________________________*/
