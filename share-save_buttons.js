1ebee4a49645c8041509a0eccb2043c578cfd6b2
Chris Koorzen <fckoorzen@gmail.com>
2024-09-20 15:05:23 +0200

    Move roadmap to seperate document.

--- share-save_buttons.js
+++ share-save_buttons.js
@@ -105,69 +105,8 @@ shareNum.addEventListener('click', () => {
 
 // Toggle visibility on click
 // NOT WORKING - ERROR!!!!!!!!
-  function aboutPopUp() {
-      document.getElementsByClassName('icon, popup').style.display = 'block';
-  }
-    //Strange must work on this more
+function aboutPopUp() {
+    document.getElementsByClassName('icon, popup').style.display = 'block';
+}
+//Strange must work on this more
 
-
-
-//_________________________________________________________________________________________________________
-// ROADMAP:
-/*
-       
-        3.  Should not be allowed to save, if number is not generated and not showing on user interface yet
-        4.  What's missing is user logins feature
-        5.  Previous winning lotto numbers - API
-        6.  Must not be able to save same or duplicate numbers
-        7.  Saving numbers generated must only be for users who pay subscription
-        8.  Add a modal close (x) and click to open modal for mobile
-        10. If statement not working - MUST FIX - No save if numbers are not generated!
-
-//_________________________________________________________________________________________________________
-// DONE SECTION
-        1.  Including map() & filter() to first six numbers, (used: slice() method) | DONE 
-        2.  Delete Saved Numbers | DONE
-        9.  List decoration must be none | DONE
-___________________________________________________________________________________________________________
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-H6X78M5G5Z"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-H6X78M5G5Z');
-</script>
-
-___________________________________________________________________________________________________________*/
-
-
-
-//_________________________________________________________________________________________________________
-           // Creator: Puso Moses Ramoroa
-          
-
-/*__________________________________________________________________________________________________________
-
-// TRIED AND TESTED 
-
-// Considered to give the locale storage saved array by date or time
-// It's giving value over same time without refreshing time
-
-    1.      const date = new Date();
-
-            let day = date.getDate();
-            let month = date.getMonth() + 1;
-            let year = date.getFullYear();
-            let second = date.getSeconds();
-
-            // This arrangement can be altered based on how we want the date's format to appear.
-            let currentDate = `${day}-${month}-${year}-${second}`;
-            console.log(currentDate); // "17-6-2022"
-
-    2.      Must probably consider using actual unique ID rather 
-            then linking that to a date saved of day-month-year
-
-_____________________________________________________________________________________________________________*/
